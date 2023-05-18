653d9d18d1d3c7dc61d0ff998780a14d1827c76c
bunny winter <bunny@wintr.family>
2023-05-18 01:15:53 -0500

    onClick capability

--- src/components/blocks/Link.tsx
+++ src/components/blocks/Link.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import React from "react";
+import React, { JSXElementConstructor } from "react";
 
 import NextLink from "next/link";
 
@@ -9,27 +9,32 @@ export const Link = ({
 	children,
 	href,
 	className,
+	onClick,
 }: {
 	children?: React.ReactNode;
-	href: string;
+	href?: string;
 	className?: string;
+	onClick?: () => void;
 }) => {
 	const squiggleStyle = {
 		"--squiggle-svg": 'url("/squiggle.svg?c=06b6d4")',
 	} as React.CSSProperties;
 
+	const Element = href ? NextLink : "a";
+
 	return (
-		<NextLink
+		<Element
 			href={href}
 			className={clsx(
 				"border-b hover:border-none hover:bg-repeat-x",
-				"text-cyan-500 border-cyan-500",
+				"text-cyan-500 border-cyan-500 cursor-pointer",
 				moduleCss.link,
 				className
 			)}
 			style={squiggleStyle}
+			onClick={onClick}
 		>
 			{children}
-		</NextLink>
+		</Element>
 	);
 };
